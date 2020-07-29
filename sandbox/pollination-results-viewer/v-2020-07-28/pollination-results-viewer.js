b551048b646a47133457a612de167a7e830af484
Theo Armour <t.armour@gmail.com>
2020-07-28 22:59:12 -0700

    links

--- sandbox/pollination-results-viewer/v-2020-07-28/pollination-results-viewer.js
+++ sandbox/pollination-results-viewer/v-2020-07-28/pollination-results-viewer.js
@@ -121,7 +121,9 @@ THRR.getHtm = function () {
 };
 
 
-function toggleDarkMode( button ) {
+const MNU = {};
+
+MNU.toggleDarkMode = function ( button ) {
 
 	if ( butDark.innerHTML === "dark" ) {
 
@@ -129,44 +131,43 @@ function toggleDarkMode( button ) {
 		document.body.style.color = "#aaa";
 		navMenu.style.backgroundColor = "#555";
 
-		THR.scene.background = new THREE.Color(0x222222);
-		THR.scene.fog.far = 999999; 
+		THR.scene.background = new THREE.Color( 0x222222 );
+		THR.scene.fog.far = 999999;
 
 		//const summaries = document.querySelectorAll(".summary-secondary");
 		//console.log( "", summaries );
 
-		Array.from( document.querySelectorAll("a") )
-		.forEach( a => a.style.color = "#ccc" );
+		Array.from( document.querySelectorAll( "a" ) )
+			.forEach( a => a.style.color = "#ccc" );
 
-		Array.from( document.querySelectorAll("input,select,option") )
-		.forEach( iso => iso.style.backgroundColor = "#bbb" );
+		Array.from( document.querySelectorAll( "input,select,option" ) )
+			.forEach( iso => iso.style.backgroundColor = "#bbb" );
 
-		document.documentElement.style.setProperty("--color-2-background", "#888");
-		Array.from( document.querySelectorAll(".summary-primary") )
-		.forEach( sum => sum.style.backgroundColor = "#888" );
-		
-		document.documentElement.style.setProperty("--color-3-background", "#bbb");
-		Array.from( document.querySelectorAll(".summary-secondary") )
-		.forEach( sum => sum.style.backgroundColor = "#bbb" );
+		document.documentElement.style.setProperty( "--color-2-background", "#888" );
+		Array.from( document.querySelectorAll( ".summary-primary" ) )
+			.forEach( sum => sum.style.backgroundColor = "#888" );
+
+		document.documentElement.style.setProperty( "--color-3-background", "#bbb" );
+		Array.from( document.querySelectorAll( ".summary-secondary" ) )
+			.forEach( sum => sum.style.backgroundColor = "#bbb" );
 
 
 		divPopUp.style.backgroundColor = "#333";
 
-		butDark.innerHTML = "light";          
+		butDark.innerHTML = "light";
 
 		return;
 
-	} 
+	}
 
-	
 	//root.style.backgroundColor = "#1e1f23";
 	document.body.style.color = "teal";
 	navMenu.style.backgroundColor = "#fafffa";
 
-	THR.scene.background = new THREE.Color(0xcce0ff);
+	THR.scene.background = new THREE.Color( 0xcce0ff );
 	THR.scene.fog.far = THR.radius * 8;
 
-	const summaries = document.querySelectorAll(".summary-primary");
+	const summaries = document.querySelectorAll( ".summary-primary" );
 	Array.from( summaries ).forEach( sum => sum.style.backgroundColor = "#eee" );
 
 	divPopUp.style.backgroundColor = "#eee";
@@ -174,3 +175,4 @@ function toggleDarkMode( button ) {
 	butDark.innerHTML = "dark";
 
 };
+

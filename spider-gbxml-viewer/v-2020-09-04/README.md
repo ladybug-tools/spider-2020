3a1224eb5854ce285e823c83268df1634391c2f5
Theo Armour <t.armour@gmail.com>
2020-09-04 10:23:33 -0700

    sgv v-2020-09-04

---
+++ spider-gbxml-viewer/v-2020-09-04/README.md
@@ -0,0 +1,45 @@
+<span style=display:none; >[You are now in a GitHub source code view - click this link to view Read Me file as a web page](https://ladybug-tools.github.io/spider-2020/#README.md "View file as a web page.") </span>
+
+<div><input type=button onclick=window.top.location.href="https://github.com/ladybug-tools/spider-2020/tree/master/spider-gbxml-viewer"
+value="You are now in a GitHub web page view - Click this button to view this read me file as source code" ></div>
+
+# [spider-gbxml-viewer.html readme]( https://www.ladybug.tools/spider-2020/#spider-gbxml-viewer/README.md )
+
+GitHub
+
+* https://ladybug-tools.github.io/spider-2020/spider-gbxml-viewer/index.html
+* https://ladybug-tools.github.io/spider-2020/spider-gbxml-viewer/readme.html
+* https://github.com/ladybug-tools/spider-2020/spider-gbxml-viewer/issues
+
+
+## Issues
+
+* 2020-09-02 ~ View menu: handle two space surfaces nicely
+* 2020-09-02 ~ View menu: Toggle all off
+* 2020-09-02 ~ View menu: id as tooltip and value
+* View menu: add context menus to select boxes
+* Data menu: button to highlight 3D surface
+* Progress indicators would be nice
+* Footer > model icon > surface types: vertical || exposed to Sun etc
+
+
+## 2020-09-03
+
+* Data menu: add scroll into view
+* 2020-09-03 ~ View menu: add global attributes
+* 2020-09-02 ~ Sample Files Gallery > view all: ignore .MD and html files
+* Sample files gallery
+  * runs until you stop it
+  * Press any key to stop it
+* 2020-09-02 ~ Data menu: update on raycast
+
+## 2020-09-02
+
+
+Done
+
+* gbXML parser: fix mis-triangulation
+
+***
+
+<center><img title="" height="32" width="32" src="https://ladybug.tools/artwork/icons_bugs/ico/spider.ico" ></center>
---
+++ spider-gbxml-viewer/v-2020-09-04/vt-view-types/vt-view-types-gbx.js
@@ -0,0 +1,262 @@
+const VT = {};
+
+
+VT.init = function () {
+
+	GBX.setSurfacesMetadata();
+
+	THR.group.children.forEach( surface => surface.userData.positionStart = surface.position.clone() );
+
+	const htm = `
+	<p>
+		Select multiple items to view. Use cursor keys to scroll through the lists.<br>
+	</p>
+	<p>
+		<button onclick="VT.resetAll();" title="Show all surfaces">&sdotb; reset visibility & position</button>
+	</p>
+
+	<details ontoggle=VTdivXmlGlobal.innerHTML=VT.getXmlGlobal() >
+
+		<summary class="summary-secondary gmd-1" >Global attributes</summary>
+
+		<div id=VTdivXmlGlobal ></div>
+
+	</details>
+
+	<div id=VTdivSurfaceTypes>${ VT.getElementSelect( GBX.surfaceTypes, "surfaceType", '<a href="https://gbxml.org/schema_doc/6.01/GreenBuildingXML_Ver6.01.html#Link1A8" target = "_blank" >Surface</a> Types') }</div>
+
+	<div id=VTdivSpaces>${ VT.getElementSelect( GBX.spaceNames, "spaceName", '<a href="https://gbxml.org/schema_doc/6.01/GreenBuildingXML_Ver6.01.html#Link19D" target="_blank">Spaces</a>' ) }</div>
+
+	<div id=VTdivStoreys>${ VT.getElementSelect( GBX.storeyNames, "storeyName", '<a href="https://gbxml.org/schema_doc/6.01/GreenBuildingXML_Ver6.01.html#Link35" target="_blank">Storeys</a>' ) }</div>
+
+	<div id=VTdivZones>${ VT.getElementSelect( GBX.zoneNames, "zoneName", '<a href="https://gbxml.org/schema_doc/6.01/GreenBuildingXML_Ver6.01.html#Link1D3" target="_blank">Zones</a>' ) }</div>
+
+	<div id=VTdivCadObjects>${ VT.getElementSelect( GBX.CADObjects, "cadObject", '<a href="https://gbxml.org/schema_doc/6.01/GreenBuildingXML_Ver6.01.html#Link3D" target="_blank">CAD Object</a>') }</div>
+
+	`;
+
+	VTdivViewTypes.innerHTML = htm;
+
+};
+
+
+VT.getXmlGlobal = function() {
+
+
+	timeStart = performance.now();
+
+	//xml = GBX.parseElement( GBX.string );
+
+
+	const parser = new DOMParser();
+	// const elementXml = parser.parseFromString( string, "text/xml" );
+	// const element = elementXml.firstChild;
+
+	xml = parser.parseFromString( GBX.string, "application/xml" ).documentElement;
+
+	const attributes = xml.attributes;
+	//console.log( "attributes", attributes );
+	const attributesHtm = Array.from( xml.attributes ).map( att => `${ att.name }: ${ att.value } <br>` ).join( "" );
+
+	//const children = xml.children;
+
+	//console.log( "childen", children );
+
+
+	//locationXml = xml.getElementsByTagName( "Location" );
+
+	//console.log( "LocationXml", LocationXml.children, LocationXml );
+
+	const locationXml = xml.getElementsByTagName( 'Location' )[ 0 ];
+	const buildingXml = xml.getElementsByTagName( 'Building' )[ 0 ];
+	//const documentXml = campusXml.getElementsByTagName( 'DocumentHistory' )[ 0 ];
+
+	const latitude = xml.getElementsByTagName( 'Latitude' )[ 0 ].innerHTML;
+	const longitude = xml.getElementsByTagName( 'Longitude' )[ 0 ].innerHTML;
+
+	let linkToMap;
+
+	if ( latitude && longitude ) {
+
+		const linkG = 'https://www.google.com/maps/@' + latitude + ',' + longitude + ',17z';
+
+		const linkW = 'http://www.wolframalpha.com/input/?i=' + latitude + '+degrees,+' + longitude + '+degrees';
+
+		linkToMap =
+			`<p>
+			<b>&raquo;</b> <a href="${ linkG }" target=_blank > &#x1f310; Google Maps</a> /
+			<a href="${ linkW }" target=_blank > Wolfram Alpha</a>
+			</p>`;
+
+	} else {
+
+		linkToMap = '';
+
+	}
+	//console.log( "ccc", locationXml, buildingXml, latitude, longitude );
+
+
+	console.log( "tim", performance.now() - timeStart );
+
+	const htm = `
+
+		${ linkToMap }
+		<p>
+			<b>gbXML Attributes</b>
+			<div>${ VT.getAttributesHtml( xml ) }</div>
+		</p>
+		<p>
+			<b>Location Attributes</b>
+			<div>${ VT.getAttributesHtml( locationXml ) }</div>
+		</p>
+		<p>
+			<b>Building Attributes</b>
+			<div>${ VT.getAttributesHtml( buildingXml ) }</div>
+		</p>
+
+		<hr>
+	`;
+
+	return htm;
+
+};
+
+
+
+
+VT.getAttributesHtml = function ( obj ) {
+	//console.log( 'obj', obj );
+
+	let htm = "";
+
+	if ( !obj ) { return htm; }
+
+	for ( let attribute of obj.attributes ) {
+		//console.log( 'attribute', attribute );
+
+		htm +=
+			`<div>
+			<span class=attributeTitle >${ attribute.name }</span>:
+			<span class=attributeValue >${ attribute.value }</span>
+		</div>`;
+
+	}
+
+	const nodes = obj.childNodes;
+	const numbers = [ 'Azimuth', 'Height', 'Tilt', 'Width' ];
+
+	for ( let node of nodes ) {
+		//console.log( 'node', node);
+
+		if ( node.nodeName !== "#text" ) {
+			//console.log( 'node', node.nodeName, node );
+
+			if ( node.childElementCount > 0 ) {
+				//console.log( 'node', node );
+
+			} else if ( node.innerHTML ) {
+				//console.log( 'node', node );
+
+				const value = numbers.includes( node.nodeName ) ? Number( node.innerHTML ).toLocaleString() : node.innerHTML;
+
+				htm +=
+
+					`<div>
+					<span class=attributeTitle >${ node.nodeName }</span>:
+					<span class=attributeValue >${ value }</span>
+				</div>`;
+
+			}
+
+		} else {
+
+			//console.log( 'node', node );
+
+		}
+
+	}
+
+	return htm;
+
+};
+
+
+
+VT.getElementSelect = function ( element = [], dataType = "surfaceType", title = "element" ) {
+
+	const htm = `
+		<p>
+			${ title } (<span>${ element.length }</span>):
+			<select id=VTsel${ dataType }  oninput=VT.showTypes(this.selectedOptions,"${ dataType }");
+				size=${ element.length < 10 ? element.length : 10 }
+				style=resize:vertical;width:100%; multiple>
+				${ element.map( ( name, index ) => `<option value=${ index } >${ name }</option>` ) }
+			</select>
+
+			<label>
+				explode: <output id=VTout${ dataType } >1</output>
+				<input type=range
+				oninput=VT.setGbxSurfaceExplode(this.value,"${ dataType }");VTout${ dataType }.value=this.value value=1 >
+			</label>
+		</p>`;
+
+	return htm;
+
+};
+
+
+
+VT.showTypes = function ( selectedOptions = "", dataType = "" ) {
+	//console.log( "type", dataType, selectedOptions );
+
+	GBX.meshes.forEach( mesh => mesh.visible = false );
+
+	for ( let option of selectedOptions ) {
+
+		GBX.meshes.filter( surface => surface.userData[ dataType ] === option.innerText )
+			.forEach( surface => surface.visible = !surface.visible );
+
+	}
+
+};
+
+
+
+
+VT.setGbxSurfaceExplode = function ( value, dataType = "" ) {
+
+	THRU.removeLines();
+
+	GBX.toggleSpaceTitles();
+
+	let items = GBX.surfaceTypes;
+	items = dataType === "spaceName" ? GBX.spaceNames : items;
+	items = dataType === "storeyName" ? GBX.storeyNames : items;
+	items = dataType === "zoneName" ? GBX.zoneNames : items;
+	items = dataType === "cadObject" ? GBX.CADObjects : items;
+	//console.log( "items", items );
+
+	items.forEach( ( item, i ) => {
+
+		GBX.meshes.filter( surface => surface.userData[ dataType ] === item )
+			.forEach( surface => surface.position.z = i * value / 5 );
+
+	} );
+
+
+};
+
+
+
+VT.resetAll = function () {
+
+	THRU.removeLines();
+
+	THR.group.children.forEach( mesh => {
+
+		mesh.visible = true;
+		mesh.position.copy( mesh.userData.positionStart );
+	} );
+
+	//VTspnCount.innerText = THR.group.children.length;
+};

5714d58bcf4e27a4862dbab1d53fe52e615944e2
Theo Armour <t.armour@gmail.com>
2020-12-21 15:43:44 -0800

    fix

--- lib-template-viewer/README.md
+++ lib-template-viewer/README.md
@@ -1,59 +1,30 @@
-<span style=display:none; >[You are now in a GitHub source code view - click this link to view Read Me file as a web page]( https://ladybug.tools/spider-2020/spider-template-viewer/readme.html "View file as a web page." ) </span>
-
-<div><input type=button class = 'btn btn-secondary btn-sm' onclick=window.location.href="https://github.com/ladybug-tools/spider-2020/tree/master/spider-template-viewer/";
-value='You are now in a GitHub web page view - Click this button to view this read me file as source code' ></div>
-
-
-# [Spider Template Viewer Read Me]( ./readme.html )
-
-_This is an actual read me. See ```assets/``` for a read me template_ 
+# [lib template viewer Read Me]( ./readme.html )
 
 <!--@@@
-<iframe src=https://ladybug.tools/spider-2020/spider-template-viewer/ width=100% height=500px >Iframes are not viewable in GitHub source code view</iframe>
-_Spider Template Viewer _
+<div class=iframe-resize ><iframe src=https://pushme-pullyou.github.io/tootoo-2020/ height=100% width=100% ></iframe></div>
+_ZZZZZ_
+
+### Full Screen: [ZZZZZ]( https://pushme-pullyou.github.io/tootoo-2020/ )
 @@@-->
 
-### Full Screen: [Spider Template Viewer]( https://www.ladybug.tools/spider-2020/spider-template-viewer/ )
+
+## Concept
 
 
-<details open >
-<summary>Concept</summary>
+## To Do / Wish List
 
 
-</details>
-
-<details open >
-<summary>To do and wish list </summary>
-
-* Real EPW viewer
-* Add viewer capability
-
-</details>
-
-<details open >
-<summary>Issues </summary>
-
-* Fog has issues
-* Heart has issues
-
-</details>
-
-<details open >
-<summary>Links of interest</summary>
+## Issues
 
 
-</details>
+## Links of Interest
 
-<details open >
-<summary>Change log </summary>
 
-### 2020-06-09
+## Change Log
 
-* First commit
+### XXX
 
-</details>
 
 ***
 
-<center title="hello! Click me to go up to the top" ><a href=javascript:window.scrollTo(0,0); > <img width=24 src="https://ladybug.tools/artwork/icons_bugs/ico/spider.ico" > </a></center>
-
+<center><a href=javascript:window.scrollTo(0,0); class=aDingbat title="Scroll to top" > ❦ </a></center>
--- lib3d-template-viewer/v-2020-12-17/lib3d-template-viewer.js
+++ lib3d-template-viewer/v-2020-12-17/lib3d-template-viewer.js
@@ -1,9 +1,11 @@
 
-const source = "https://github.com/theo-armour/2020/tree/master/lib-template-viewer/";
+const source = "https://github.com/pushme-pullyou/too-too-2020/tree/master/lib3d-template-viewer"
 
-const version = "v-2020-11-28";
+const version = "v-2020-12-17";
 
 const description = document.head.querySelector( "[ name=description ]" ).content;
+
+const icon = `<img style=color: teal; title = "Your AEC 3D viewer happy place!" height = "24" width = "24"; src = "https://ladybug.tools/artwork/icons_bugs/ico/spider.ico" >`
 
 //const urlDefault = "README.md";
 
@@ -14,6 +16,10 @@ function init () {
 	spnVersion.innerHTML = version;
 
 	divDescription.innerHTML = description;
+
+	aMenuFooter.innerHTML = icon;
+
+
 
 
 	THR.init();
@@ -42,136 +48,19 @@ function init () {
 
 	// JTV.init();
 
-	HRT.init();
+	//HRT.init();
 
-};
+	// if running on server, keeps address bar pointed to latest dev
 
+	if ( !location.hash && location.protocol === "https:" ) {
 
+		window.history.pushState( "", "", "../" );
 
-FOO.onLoadFile = function () {
+	} else {
 
-	//console.log( "FOO.string", FOO.string.slice( 0, 50 ) );
-
-	const jsonArr = FOO.string;
-
-	let x = 0;
-	const half = jsonArr.length / 24;
-
-	const dataPoints = jsonArr.map( ( item, index ) => {
-
-		//console.log( "item", item );
-
-		x = ( index % 12 === 0 ) ? ++x : x;
-
-		const geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
-		const material = new THREE.MeshNormalMaterial();
-		const mesh = new THREE.Mesh( geometry, material );
-		mesh.position.set( ( x - half ) * 3, item.month * 5, item.surface_inside_face_temperature );
-		mesh.scale.z = ( item.surface_outside_face_temperature - item.surface_inside_face_temperature );
-
-		return mesh;
-
-	} );
-
-	THR.group.add( ...dataPoints );
-
-	//console.log( "", dataPoints );
-
-	THR.updateScene();
-
-	THRR.updateScene();
-
-	THR.camera.position.set( -100, -100, 100 );
-
-	if ( window.divPopUp ) {
-
-		divPopUp.hidden = false;
-		//divPopUp.innerText = "FOO.string\n" + FOO.string.slice( 0, 50 );
+		THR.controls.autoRotate = false;
 
 	}
 
 };
 
-
-
-THRR.vvvgetHtm = function () {
-
-	//console.log( "xxxx intersected", THRR.intersected );
-	const mesh = THRR.intersected.object;
-
-	const index = THR.group.children.indexOf( mesh );
-
-	const item = FOO.string[ index ];
-
-	//console.log( "item", item );
-
-	const htm = `
-	<div>
-		index: ${ index }<br>
-		identifier: ${ item.identifier }<br>
-		year: ${ item.year } month: ${ item.month } day: ${ item.day }</br>
-		hour: ${ item.hour } minute: ${ item.minute }</br>
-		inside temperature: ${ item.surface_inside_face_temperature.toLocaleString() }</br>
-		outside temperature: ${ item.surface_outside_face_temperature.toLocaleString() }</br>
-
-	</div>`;
-
-	return htm;
-};
-
-
-
-const MNU = {};
-
-MNU.toggleDarkMode = function ( button ) {
-
-	if ( butDark.innerHTML === "dark" ) {
-
-		//root.style.backgroundColor = "#1e1f23";
-		document.body.style.color = "#aaa";
-		navMenu.style.backgroundColor = "#555";
-
-		THR.scene.background = new THREE.Color( 0x222222 );
-		THR.scene.fog.far = 999999;
-
-		//const summaries = document.querySelectorAll(".summary-secondary");
-		//console.log( "", summaries );
-
-		Array.from( document.querySelectorAll( "a" ) )
-			.forEach( a => a.style.color = "#ccc" );
-
-		Array.from( document.querySelectorAll( "input,select,option" ) )
-			.forEach( iso => iso.style.backgroundColor = "#bbb" );
-
-		document.documentElement.style.setProperty( "--color-2-background", "#888" );
-		Array.from( document.querySelectorAll( ".summary-primary" ) )
-			.forEach( sum => sum.style.backgroundColor = "#888" );
-
-		document.documentElement.style.setProperty( "--color-3-background", "#bbb" );
-		Array.from( document.querySelectorAll( ".summary-secondary" ) )
-			.forEach( sum => sum.style.backgroundColor = "#bbb" );
-
-
-		divPopUp.style.backgroundColor = "#333";
-
-		butDark.innerHTML = "light";
-
-		return;
-
-	}
-
-	//root.style.backgroundColor = "#1e1f23";
-	document.body.style.color = "teal";
-	navMenu.style.backgroundColor = "#fafffa";
-
-	THR.scene.background = new THREE.Color( 0xcce0ff );
-	THR.scene.fog.far = THR.radius * 8;
-
-	const summaries = document.querySelectorAll( ".summary-primary" );
-	Array.from( summaries ).forEach( sum => sum.style.backgroundColor = "#eee" );
-
-	divPopUp.style.backgroundColor = "#eee";
-
-	butDark.innerHTML = "dark";
-
-};
